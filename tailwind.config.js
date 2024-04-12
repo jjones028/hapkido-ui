5f290392171eaeb183b3322ad69fbd73e10c7f5b
jeremy.jones <jjones@stewartsshops.local>
2024-04-12 16:52:29 -0400

    Testing out some stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
-import {nextui} from "@nextui-org/react";
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    // "./src/**/*.{html,js}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [nextui(
-      {
+    content: [
+        "./src/**/*.{html,js,jsx,ts,tsx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+    plugins: [nextui({
         prefix: "nextui",
         defaultTheme: "dark"
-      }
-  )],
+    })],
 }
 
