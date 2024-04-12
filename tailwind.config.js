62747f2d308991ff4ee3862f4f7ceb6fbe7e65d9
jeremy.jones <jjones@stewartsshops.local>
2024-04-12 14:57:36 -0400

    Setup nextui

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import {nextui} from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // "./src/**/*.{html,js}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [nextui(
+      {
+        prefix: "nextui",
+        defaultTheme: "dark"
+      }
+  )],
+}
+
