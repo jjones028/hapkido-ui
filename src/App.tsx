c3c55170fd95389a435c7b52fa76eb38c9987774
jeremy.jones <jjones@stewartsshops.local>
2024-04-25 16:36:46 -0400

    Add a NavBar and Brand and Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import './App.css'
-import {Button, Card, CardBody, Navbar} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent} from "@nextui-org/react";
+import {Logo} from "./Logo.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    {/*const [count, setCount] = useState(0)*/}
     {/*<Card>*/}
     {/*    <CardBody>*/}
     {/*         <p>Make beautiful websites regardless of your design experience.</p>*/}
@@ -14,26 +13,20 @@ function App() {
     {/*     </CardBody>*/}
     {/* </Card>*/}
 
-  return (
+    const elements = [...Array(9).keys()].map(i=> ++i);
+    return (
     <>
-        <Navbar className="bg-blue-500"></Navbar>
+        <Navbar maxWidth="full">
+            <NavbarBrand>
+                <Logo />
+                <p className="font-bold text-inherit">Spektacle.io</p>
+            </NavbarBrand>
+            <NavbarContent />
+        </Navbar>
         <div className="grid grid-cols-3 grid-rows-3 gap-3">
-            <div className="p-4 bg-teal-500 shadow-lg rounded-lg hover:bg-teal-900">1</div>
-            <div className="p-4 bg-teal-500 shadow-lg rounded-lg">2</div>
-            <div className="p-4 bg-teal-500 shadow-lg rounded-lg">3</div>
-            <div className="p-4 bg-teal-500 shadow-lg rounded-lg">4</div>
-            <Card>
-                <CardBody>
-                     <p>Make beautiful websites regardless of your design experience.</p>
-                     <Button color="primary" onPress={() => setCount((count) => count + 1)}>
-                         Count is {count}
-                     </Button>
-                 </CardBody>
-             </Card>
-            <div className="p-4 bg-teal-500 shadow-lg rounded-lg">6</div>
-            <div className="p-4 bg-teal-500 shadow-lg rounded-lg">7</div>
-            <div className="p-4 bg-teal-500 shadow-lg rounded-lg">8</div>
-            <div className="p-4 bg-teal-500 shadow-lg rounded-lg">9</div>
+            { elements.map((i) => (
+                <div key={i} className="p-4 bg-teal-500 shadow-lg rounded-lg hover:bg-teal-900">{i}</div>
+            ))}
         </div>
     </>
   )
