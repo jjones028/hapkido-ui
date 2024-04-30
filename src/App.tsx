7ec72faf26be0f8c7236b4d49966598343d53dff
jeremy.jones <jjones@stewartsshops.local>
2024-04-30 15:27:38 -0400

    Add a modal to start adding the ability to edit the subjectNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import {useEffect, useState} from "react"
 import AppBar from "./components/AppBar.tsx"
 import SubjectNameCard from "./components/SubjectNameCard.tsx"
 import {SubjectName} from "./interfaces/SubjectName.ts"
+import {Button, Modal, ModalBody, ModalContent, ModalFooter} from "@nextui-org/react";
 
 
 
 function App() {
     const [elements, setElements] = useState<SubjectName[]>([])
+    const [editDialogIsOpen, setEditDialogIsOpen] = useState<boolean>(false)
 
     useEffect(() => {
         const getElements = async () => {
@@ -20,10 +22,19 @@ function App() {
 
     return (
         <>
+            <Modal isOpen={editDialogIsOpen} onClose={() => setEditDialogIsOpen(false)}>
+                <ModalContent>
+                    <ModalBody></ModalBody>
+                    <ModalFooter>
+                        <Button className="bg-primary text-background" onClick={() => setEditDialogIsOpen(false)}>Save</Button>
+                        <Button className="bg-danger-200" onClick={() => setEditDialogIsOpen(false)}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
             <AppBar />
             <div className="grid grid-cols-3 grid-rows-3 gap-3">
                 { elements.map((subjectName) => (
-                    <SubjectNameCard key={subjectName.id} subjectName={subjectName} />
+                    <SubjectNameCard key={subjectName.id} subjectName={subjectName} onClick={() => setEditDialogIsOpen(true)} />
                 )) }
             </div>
         </>
